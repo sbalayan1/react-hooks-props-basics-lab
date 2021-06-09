4512a0296adb8ce9a61a7ff3672fcbe39e806a09
sbalayan1 <balayans2014@yahoo.com>
2021-06-08 17:18:33 -0700

    commit tree af83c525dd79ea321f3a9c4ccb7239aef922df3c

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,6 @@ function About(props) {
                     <h2>About Me</h2>
                     <p>{props.bio}</p>
                     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-                    {/* add your <Links /> component here */}
 
                     <Links 
                         github = {user.links.github}
@@ -23,7 +22,6 @@ function About(props) {
             <div>
                 <h2>About Me</h2>
                 <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-                    {/* add your <Links /> component here */}
 
                     <Links 
                         github = {user.links.github}
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 let Links = (props) => {
-    console.log(props)
     return(
         <div>
             <h3>Links</h3>
